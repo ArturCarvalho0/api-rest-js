ba7f5084e6e95ef947c526ee5c357bc9553d4d84
Artur Carvalho <artur.carvalho0@outlook.com>
2023-08-31 16:12:10 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-unused-vars': 'off',
+    'class-methods-use-this': 'off',
+  },
+};
