1fc4f7b22027b3203a2973701e432dca62279d71
Artur Carvalho <artur.carvalho0@outlook.com>
2023-09-05 16:06:14 -0300

    CRUD de alunos

--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import AlunoController from '../controllers/AlunoController';
 
+import loginRequired from '../middlewares/loginRequired';
+
 const router = new Router();
 
 router.get('/', AlunoController.index);
+router.get('/:id', AlunoController.show);
+router.post('/', loginRequired, AlunoController.store);
+router.put('/:id', loginRequired, AlunoController.update);
+router.delete('/:id', loginRequired, AlunoController.delete);
 
 export default router;
